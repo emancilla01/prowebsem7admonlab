03ca9fb008bac90580fa5ccae1bdec6821e7a028
emancilla01 <emancilla01@gmail.com>
2025-11-02 16:22:33 -0600

    fixed title in description

--- resources/views/inicio2.blade.php
+++ resources/views/inicio2.blade.php
@@ -4,7 +4,7 @@
 @endsection
 @section('contenido2')
 <div class="d-flex justify-content-center align-items-center" style="height: 80vh;">
-    <h3>Bienvenido a I/O, un sistema diseñado para facilitar la gestión y control de
+    <h3>Bienvenido a IO, un sistema diseñado para facilitar la gestión y control de
         equipos de cómputo. Permite registrar, monitorear y mantener actualizado el
         inventario tecnológico de manera eficiente, segura y organizada.
 </div>
