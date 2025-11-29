b182a5724b5811927ff48759689a891b377cc507
emancilla01 <emancilla01@gmail.com>
2025-11-28 20:32:53 -0600

    cleared cache

--- storage/framework/views/32d8f79cd989ce7bfe21afe812a31563.php
+++
@@ -1,9 +0,0 @@
-<?php $__env->startSection('menu'); ?>
-    <?php echo $__env->make('logmenu', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?>
-<?php $__env->stopSection(); ?>
-<?php $__env->startSection('contenido'); ?>
-<div class="d-flex justify-content-center align-items-center" style="height: 100vh;">
-    <h1>Bienvenido al sistema IO</h1>
-</div>
-<?php $__env->stopSection(); ?> 
-<?php echo $__env->make('plantillas.login', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH C:\Users\Usuario\Herd\admonlab\resources\views/inicio.blade.php ENDPATH**/ ?>
