bf37dde153e762685316efe85407053c10d659b6
emancilla01 <emancilla01@gmail.com>
2025-12-06 21:07:25 -0600

    project and everything after is merged here into one

--- storage/framework/views/624c5e73a90db78ba69469400185c456.php
+++ storage/framework/views/624c5e73a90db78ba69469400185c456.php
@@ -1,5 +1,3 @@
-
-
 <?php $__env->startSection('menu2'); ?>
     <?php echo $__env->make('menu2', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?>
 <?php $__env->stopSection(); ?>
